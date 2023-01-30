3d95278e65171bebd508db1683046859186d6235
DaveFullStack <119569407+DaveFullStack@users.noreply.github.com>
2023-01-30 17:29:09 +0000

    created function for computer choice calling on array of game choices using randomInt Function.

--- javascript/app.js
+++ javascript/app.js
@@ -1,11 +1,19 @@
+let computerScore = 0
+let playerScore = 0
 
-function getRandomInt (max) {
+
+
+//creating function to get a random int between specified operands.
+function getRandomInt(max) {
     return Math.floor(Math.random() * max )
 }
-console.log(getRandomInt(3))
+
+//console.log(getRandomInt(3))
+//testing function getRandomInt
+
 const gameChoice = ['rock', 'paper', 'scissors']
-// creating array of possible game choices. Will use index to call upon 0, 1 || 2.
 
-function getComputerChoice() {
-
+function getComputerChoice(arrayIndex = getRandomInt(3)) {
+    return gameChoice[arrayIndex]
 }
+console.log(getComputerChoice())
