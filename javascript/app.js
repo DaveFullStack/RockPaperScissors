889213878ee5b9d5146b18a8a123b45982bd717a
DaveFullStack <119569407+DaveFullStack@users.noreply.github.com>
2023-01-30 17:53:27 +0000

    created function for getting player choice. using if and else if to assign answer to gameChoice array.

--- javascript/app.js
+++ javascript/app.js
@@ -12,8 +12,27 @@ function getRandomInt(max) {
 //testing function getRandomInt
 
 const gameChoice = ['rock', 'paper', 'scissors']
+//creating array so I can use index to assign values to functions getPlayerChoice && getComputerChoice
+
+function getPlayerChoice() {
+    let playerChoice = prompt('please enter Rock, Paper or Scissors!').toLowerCase();
+    //creating variable which is assigned through prompt.
+    if (playerChoice === 'rock') {
+        playerChoice = gameChoice[0]
+    }
+    else if (playerChoice === 'paper') {
+        playerChoice = gameChoice[1]
+    }
+    else if (playerChoice === 'scissors') {
+        playerChoice = gameChoice[2]
+    }
+    return playerChoice
+}
+
+//console.log(getPlayerChoice())
 
 function getComputerChoice(arrayIndex = getRandomInt(3)) {
     return gameChoice[arrayIndex]
 }
 console.log(getComputerChoice())
+
