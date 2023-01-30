fcda60c55dcaab660f6447794d94aa4f71223b1a
DaveFullStack <119569407+DaveFullStack@users.noreply.github.com>
2023-01-30 17:00:37 +0000

    Created function to get random number. using random number to select index of array for game choices (rock, paper, scissors) git push

---
+++ javascript/app.js
@@ -0,0 +1,11 @@
+
+function getRandomInt (max) {
+    return Math.floor(Math.random() * max )
+}
+console.log(getRandomInt(3))
+const gameChoice = ['rock', 'paper', 'scissors']
+// creating array of possible game choices. Will use index to call upon 0, 1 || 2.
+
+function getComputerChoice() {
+
+}
